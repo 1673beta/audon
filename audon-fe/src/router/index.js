5241baf80d7b60d7b320270a8c7633c5d7ecdd45
Namekuji <nmkj@mx.a-ef.org>
2022-12-30 00:31:56 +0900

    add noauth to notfound view

--- audon-fe/src/router/index.js
+++ audon-fe/src/router/index.js
@@ -7,7 +7,14 @@ import NotFoundView from "../views/NotFoundView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/:pathMatch(.*)*", name: "notfound", component: NotFoundView },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "notfound",
+      meta: {
+        noauth: true,
+      },
+      component: NotFoundView,
+    },
     {
       path: "/",
       name: "home",
