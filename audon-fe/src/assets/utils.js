0888bf758efa95b4e900e8793da3746046ac20bd
Namekuji <nmkj@mx.a-ef.org>
2023-01-25 14:58:17 -0500

    fix fqdn validation error #28

--- audon-fe/src/assets/utils.js
+++ audon-fe/src/assets/utils.js
@@ -3,8 +3,7 @@ import router from "../router";
 
 export const validators = {
   fqdn: helpers.regex(
-    /^[a-zA-Z]([a-zA-Z0-9-]+[.]?)*[a-zA-Z0-9]$/,
-    /^([a-zA-Z0-9]{1}[a-zA-Z0-9-]{0,62})(\.[a-zA-Z0-9]{1}[a-zA-Z0-9-]{0,62})*?(\.[a-zA-Z]{1}[a-zA-Z0-9]{0,62})\.?$/
+    /(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)/
   ),
 };
 
